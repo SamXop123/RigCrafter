1f95a342c2bdd9a40ccccf8200e67d42265f6621
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-04-26 22:01:01 +0530

    Update table.tsx

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -81,3 +81,28 @@ const TableHead = React.forwardRef<
 ))
 TableHead.displayName = "TableHead"
 
+const TableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <td
+    ref={ref}
+    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    {...props}
+  />
+))
+TableCell.displayName = "TableCell"
+
+const TableCaption = React.forwardRef<
+  HTMLTableCaptionElement,
+  React.HTMLAttributes<HTMLTableCaptionElement>
+>(({ className, ...props }, ref) => (
+  <caption
+    ref={ref}
+    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+TableCaption.displayName = "TableCaption"
+
+
